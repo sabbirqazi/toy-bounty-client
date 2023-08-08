7f3ccbfc3b35971ab01005fa6ced55e787029d07
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-08-08 19:25:43 +0600

    newsletter added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import useTitle from "../../../hooks/useTitle";
 import Banner from "../Banner/Banner";
 import Gallary from "../Gallary/Gallary";
 import LatestToy from "../LatestToy/LatestToy";
+import NewsLetter from "../NewsLetter/NewsLetter";
 import Offer from "../Offer/Offer";
 import ShopByCategory from "../ShopByCategory/ShopByCategory";
 
@@ -19,7 +20,7 @@ const Home = () => {
             <ShopByCategory></ShopByCategory>
             <Offer></Offer>
             <LatestToy></LatestToy>
-            
+            <NewsLetter></NewsLetter>
         </div>
     );
 };
